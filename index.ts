8320689853775ae2a64089e7fa7ac7148b24cbf7
hyeyoung <swim070@samwooim.com>
2021-07-13 12:09:36 +0900

    no message

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Hyeyoung",
+    age = 23,
+    gender = "female";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
