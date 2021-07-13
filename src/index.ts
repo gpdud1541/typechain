f8404aa95cca1e77aeb3fc8f757ef05691a27b99
hyeyoung <swim070@samwooim.com>
2021-07-13 14:48:14 +0900

    interface

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+    name: string,
+    age: number,
+    gender: string;
+}
+
+const person = {
+    name: "name",
+    age: 22,
+    gender: "male"
 };
 
-console.log(sayHi("Hyeyoung", 23, "female"));
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+};
+
+console.log(sayHi(person));
 
 export {};
